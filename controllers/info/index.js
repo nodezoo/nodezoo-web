a25e8509a10e795791df336bb1b7176525330200
naomi feehan moran <naomi.feehan@nearform.com>
2014-09-05 11:26:11 +0100

    Bootstrapped

--- controllers/info/index.js
+++ controllers/info/index.js
@@ -8,19 +8,13 @@ module.exports = function (router) {
 
   router.get('/:mod', function (req, res) {
     var mod = req.params.mod
-    console.log("++++++++++++");
-    console.log(mod);
 
     req.seneca.act('role:info,cmd:get',{name:mod}, function(err,mod){
       if( err ) return res.status(500).end();
-      if( !mod ) return res.status(404).end();
 
       var model = {
         mod:  mod
       }
-      console.log("****************");
-
-      console.log(mod)
 
       res.render('info', model);
     })
