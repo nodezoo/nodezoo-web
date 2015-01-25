ebf2021909a47877750c1774d85367aaca671b80
Peter Elger <peter.elger@nearform.com>
2015-01-25 10:36:18 +0000

    added support for beanstalk

--- seneca-init.js
+++ seneca-init.js
@@ -1,23 +1,18 @@
-"use strict";
+'use strict';
 
-var seneca = require('seneca')
-
-
-var api = seneca()
+var seneca = require('seneca');
+var api = seneca();
 
 
-if( 'dev' != api.options().env ) {
-      
+if( 'dev' !== api.options().env ) {
   api
     .use('./lib/api.js')
-
-    .client({host: '192.168.59.103', port:9100,pin:'role:info'})
-    .client({host: '192.168.59.103', port:9003,pin:'role:search'})
-
-    .listen(9000)
+    .use('beanstalk-transport')
+    .client({host: process.env.BEANSTALK_IP, type: 'beanstalk', port:1130, pin:'role:info,cmd:*'})
+    .client({host: process.env.BEANSTALK_IP, type: 'beanstalk', port:1130, pin:'role:search,cmd:*'})
+    .listen({host: process.env.BEANSTALK_IP, type: 'beanstalk', port:1130});
 }
 else {
-
   api
     .use('jsonfile-store',{folder:__dirname+'/data'})
     .use('../nodezoo-github')
@@ -27,27 +22,28 @@ else {
     .use('./lib/api.js')
   
     .add('role:info,req:part',function(args,done){
-      done()
+      done();
       
       this.act(
         'role:npm,cmd:get',
         {name:args.name},
         function(err,mod){
           this.act('role:info,res:part,part:npm',
-                   {name:args.name,data:mod})
+                   {name:args.name,data:mod});
             
           this.act(
             'role:github,cmd:get',
             {name:args.name,giturl:mod.giturl},
             function(err,mod){
               this.act('role:info,res:part,part:github',
-                       {name:args.name,data:mod})
-            })
-        })
-    })
+                       {name:args.name,data:mod});
+            });
+        });
+    });
 }
 
 
 module.exports = function(){
-  return api.export('web')
-}
+  return api.export('web');
+};
+
