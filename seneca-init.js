a8ac69448375bd18fa31ba292ae541ea1d9464dd
Richard Rodger <richard@ricebridge.com>
2015-10-01 17:02:59 +0100

    updated

--- seneca-init.js
+++ seneca-init.js
@@ -2,50 +2,15 @@
 
 var seneca = require('seneca')
 
-
 var api = seneca()
 
+api
+  .use('./lib/api.js')
 
-if( 'dev' != api.options().env ) {
-      
-  api
-    .use('./lib/api.js')
+  .client({port:9100,pin:'role:info'})
+  .client({port:9003,pin:'role:search'})
 
-    .client({port:9100,pin:'role:info'})
-    .client({port:9003,pin:'role:search'})
-
-    .listen(9000)
-}
-else {
-
-  api
-    .use('jsonfile-store',{folder:__dirname+'/data'})
-    .use('../nodezoo-github')
-    .use('../nodezoo-npm')
-    .use('../nodezoo-info')
-    .use('../nodezoo-index')
-    .use('./lib/api.js')
-  
-    .add('role:info,req:part',function(args,done){
-      done()
-      
-      this.act(
-        'role:npm,cmd:get',
-        {name:args.name},
-        function(err,mod){
-          this.act('role:info,res:part,part:npm',
-                   {name:args.name,data:mod})
-            
-          this.act(
-            'role:github,cmd:get',
-            {name:args.name,giturl:mod.giturl},
-            function(err,mod){
-              this.act('role:info,res:part,part:github',
-                       {name:args.name,data:mod})
-            })
-        })
-    })
-}
+  .listen(9000)
 
 
 module.exports = function(){
