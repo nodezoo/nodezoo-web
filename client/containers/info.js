6bc980be4618cd0aea505bb0f71d7dc778a252af
Dean McDonnell <mcdonnelldean@outlook.com>
2016-01-25 12:42:41 +0000

    Info fix

--- client/containers/info.js
+++ client/containers/info.js
@@ -29,7 +29,7 @@ export const Info = React.createClass({
       )
     }
     else {
-      const {github, npm} = this.props.result
+      const {no_github, github, no_npm, npm} = this.props.result
 
       body = (
         <div className="col-xs-12 col-sm-8 col-md-8 col-rg-6">
@@ -41,25 +41,36 @@ export const Info = React.createClass({
           </div>
 
           <div className="panel panel-module-info txt-left">
-            <div className="panel-module">
-              <h2 className="mt0"><span className="logo logo-npm"></span> npm</h2>
-              <ul className="list-unstyled cf module-info-list">
-                <li><strong className="module-info-heading">Tagline:</strong> {npm.desc}</li>
-                <li><strong className="module-info-heading">Version:</strong> {npm.version}</li>
-              </ul>
-            </div>
+            {(() => {
+              if (!no_npm) {
+                return (
+                  <div className="panel-module">
+                    <h2 className="mt0"><span className="logo logo-npm"></span> npm</h2>
+                    <ul className="list-unstyled cf module-info-list">
+                      <li><strong className="module-info-heading">Tagline:</strong> {npm.desc}</li>
+                      <li><strong className="module-info-heading">Version:</strong> {npm.version}</li>
+                    </ul>
+                  </div>
+                )
+              }
+            })()}
 
-            <div className="panel-module">
-              <h2 className="mt0"><span className="logo logo-git"></span> Github</h2>
-
-              <ul className="list-unstyled module-info-list cf">
-                <li><strong className="module-info-heading">Created:</strong> {github.last}</li>
-                <li><strong className="module-info-heading">URL:</strong><a href="#"> {npm.giturl}</a></li>
-                <li><strong className="module-info-heading">Watches:</strong> {github.watches}</li>
-                <li><strong className="module-info-heading">Forks:</strong> {github.forks}</li>
-                <li><strong className="module-info-heading">Stars:</strong> {github.stars}</li>
-              </ul>
-            </div>
+            {(() => {
+              if (!no_github) {
+                return (
+                  <div className="panel-module">
+                    <h2 className="mt0"><span className="logo logo-git"></span> Github</h2>
+                    <ul className="list-unstyled module-info-list cf">
+                      <li><strong className="module-info-heading">Created:</strong> {github.last}</li>
+                      <li><strong className="module-info-heading">URL:</strong><a href="#"> {npm.giturl}</a></li>
+                      <li><strong className="module-info-heading">Watches:</strong> {github.watches}</li>
+                      <li><strong className="module-info-heading">Forks:</strong> {github.forks}</li>
+                      <li><strong className="module-info-heading">Stars:</strong> {github.stars}</li>
+                    </ul>
+                  </div>
+                )
+              }
+            })()}
           </div>
         </div>
       )
