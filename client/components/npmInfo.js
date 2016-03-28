48429b11fcf7527366fd16a15e8e6b5c9622a5b0
Gege Pincin <gege@pincin.com>
2016-03-28 15:17:13 -0400

    fixing jacked up spacing

--- client/components/npmInfo.js
+++ client/components/npmInfo.js
@@ -6,16 +6,16 @@ export const NpmInfo = React.createClass({
 	propTypes: {
     	npm: React.PropTypes.object.isRequired
   	},
+
 	render () {
 		return (
 			<div className="panel-module">
-		        <h2 className="mt0"><span className="logo logo-npm"></span> npm</h2>
-		        <ul className="list-unstyled cf module-info-list">
+		  	<h2 className="mt0"><span className="logo logo-npm"></span> npm</h2>
+	    	<ul className="list-unstyled cf module-info-list">
 					<li><strong className="module-info-heading">Tagline:</strong> {this.props.npm.desc}</li>
 					<li><strong className="module-info-heading">Version:</strong> {this.props.npm.version}</li>
-		        </ul>
-	  		</div>
-
+        </ul>
+  		</div>
 		)
 	}
 })
