3f0a3b1b2b15e0a99402428e8960932d4707d344
Gege Pincin <gege@pincin.com>
2016-03-28 21:43:11 -0400

    adding git clone URL

--- client/components/gitInfo.js
+++ client/components/gitInfo.js
@@ -15,6 +15,7 @@ export const GitInfo = React.createClass({
           <li><strong className="module-info-heading">URL:</strong>
             <a href={this.props.github.url} target="_blank"> {this.props.github.url}</a>
           </li>
+          <li><strong className="module-info-heading">Clone URL: </strong> {this.props.github.gitClone}</li>
           <li><strong className="module-info-heading">Watches:</strong> {this.props.github.watches}</li>
           <li><strong className="module-info-heading">Forks:</strong> {this.props.github.forks}</li>
           <li><strong className="module-info-heading">Stars:</strong> {this.props.github.stars}</li>
