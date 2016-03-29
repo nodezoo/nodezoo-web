38117339d1328dbca2274c421c78b21d8b54964f
Gege Pincin <gege@pincin.com>
2016-03-28 23:47:02 -0400

    added copy to clipboard feature

--- client/components/gitInfo.js
+++ client/components/gitInfo.js
@@ -1,6 +1,7 @@
 'use strict'
 
 import React from 'react'
+import CopyToClipboard from 'react-copy-to-clipboard';
 
 export const GitInfo = React.createClass({
   propTypes: {
@@ -15,7 +16,13 @@ export const GitInfo = React.createClass({
           <li><strong className="module-info-heading">URL:</strong>
             <a href={this.props.github.url} target="_blank"> {this.props.github.url}</a>
           </li>
-          <li><strong className="module-info-heading">Clone URL: </strong> {this.props.github.gitClone}</li>
+          <li>
+            <strong className="module-info-heading">Clone URL:</strong>
+              &nbsp;{this.props.github.gitClone} &nbsp;
+            <CopyToClipboard text={this.props.github.gitClone}>
+              <button className="btn">Copy</button>
+            </CopyToClipboard>
+          </li>
           <li><strong className="module-info-heading">Watches:</strong> {this.props.github.watches}</li>
           <li><strong className="module-info-heading">Forks:</strong> {this.props.github.forks}</li>
           <li><strong className="module-info-heading">Stars:</strong> {this.props.github.stars}</li>
