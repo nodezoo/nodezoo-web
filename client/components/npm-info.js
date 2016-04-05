62cb7677d9e49cb68f9adb450c2138d2798309da
Gege Pincin <gege@pincin.com>
2016-04-05 02:47:43 -0400

    seriously this file...lint

--- client/components/npm-info.js
+++ client/components/npm-info.js
@@ -3,20 +3,20 @@
 import React from 'react'
 
 export const NpmInfo = React.createClass({
-	propTypes: {
-		npm: React.PropTypes.object.isRequired
-	},
-	render () {
-		return (
-			<div className="panel-module">
-				<h2 className="mt0"><span className="logo logo-npm"></span> npm</h2>
-				<ul className="list-unstyled cf module-info-list">
-					<li><strong className="module-info-heading">Tagline:</strong> {this.props.npm.desc}</li>
-					<li><strong className="module-info-heading">Version:</strong> {this.props.npm.version}</li>
-				</ul>
-			</div>
-		)
-	}
+  propTypes: {
+    npm: React.PropTypes.object.isRequired
+  },
+  render () {
+    return (
+      <div className="panel-module">
+        <h2 className="mt0"><span className="logo logo-npm"></span> npm</h2>
+        <ul className="list-unstyled cf module-info-list">
+          <li><strong className="module-info-heading">Tagline:</strong> {this.props.npm.desc}</li>
+          <li><strong className="module-info-heading">Version:</strong> {this.props.npm.version}</li>
+          </ul>
+      </div>
+    )
+  }
 })
 
 export default NpmInfo
