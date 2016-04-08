d611f58269bca1c9254f1b6ad28d1a18a18ee808
Shane Lacey <shane.lacey@nearform.com>
2016-04-08 14:19:16 +0100

    Added project, clone and ssh url to github info

--- client/components/git-info.js
+++ client/components/git-info.js
@@ -8,14 +8,22 @@ export const GitInfo = React.createClass({
     github: React.PropTypes.object.isRequired
   },
   getInitialState: function () {
-    return {showPopUp: false}
+    return {showClonePopUp: false,
+            showSshPopUp: false}
   },
-  transitionCopy: function () {
+  transitionCloneCopy: function () {
     var self = this
     // might want to bring in react-addons transition group for this
-    this.setState({showPopUp: true})
+    this.setState({showClonePopUp: true})
     setTimeout(function () {
-      self.setState({showPopUp: false})
+      self.setState({showClonePopUp: false})
+    }, 500)
+  },
+  transitionSshCopy: function () {
+    var self = this
+    this.setState({showSshPopUp: true})
+    setTimeout(function () {
+      self.setState({showSshPopUp: false})
     }, 500)
   },
   render () {
@@ -30,13 +38,22 @@ export const GitInfo = React.createClass({
           <li><strong className="module-info-heading">Forks:</strong> {this.props.github.forks}<span className="icon icon-fork-sm"></span></li>
           <li><strong className="module-info-heading">Watches:</strong> {this.props.github.watches}<span className="icon icon-eye-sm"></span></li>
           <li><strong className="module-info-heading">Last commit:</strong> {this.props.github.last}</li>
+          <li><strong className="module-info-heading">Project URL:</strong> <a href={this.props.github.urlRepo}>{this.props.github.urlRepo}</a></li>
           <li>
-            <strong className="module-info-heading">Repo URL:</strong>
-            &nbsp;{this.props.github.urlRepo} &nbsp;
-            <CopyToClipboard text={this.props.github.urlRepo}>
-              <span><a onClick={this.transitionCopy} className="icon-clipboard"></a></span>
+            <strong className="module-info-heading">Clone URL:</strong>
+            &nbsp;{this.props.github.urlClone} &nbsp;
+            <CopyToClipboard text={this.props.github.urlClone}>
+              <span><a onClick={this.transitionCloneCopy} className="icon-clipboard"></a></span>
             </CopyToClipboard>
-            <span className={this.state.showPopUp ? 'visible' : 'hidden'}> Copied!</span>
+            <span className={this.state.showClonePopUp ? 'visible' : 'hidden'}> Copied!</span>
+          </li>
+          <li>
+            <strong className="module-info-heading">SSH URL:</strong>
+            &nbsp;{this.props.github.urlSsh} &nbsp;
+            <CopyToClipboard text={this.props.github.urlSsh}>
+              <span><a onClick={this.transitionSshCopy} className="icon-clipboard"></a></span>
+            </CopyToClipboard>
+            <span className={this.state.showSshPopUp ? 'visible' : 'hidden'}> Copied!</span>
           </li>
           <li><strong className="module-info-heading">Open PR's:</strong> {this.props.github.pullRequests}</li>
         </ul>
