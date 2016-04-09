a485325950116161a665c877ef09ee71ab856644
Vladimir Islamov <vlad@moreboats.com>
2016-04-08 18:18:14 -0700

    Work in progress #68

--- client/components/search-result.js
+++ client/components/search-result.js
@@ -13,14 +13,12 @@ export const SearchResult = React.createClass({
     const result = this.props.data
     const infoUrl = `/info/${result.name}`
     return (
-      <div id="query-results-container" className="panel col-xs-12 col-sm-10 col-md-8 col-rg-6">
-        <span className="icon icon-hex-4"></span>
+      <div className="query-results-container col-xs-12 col-sm-10 col-md-8 col-rg-6">
         <div className="cf">
-          <h3 className="m0 fl-left">
-            <a className="txt-uppercase" target="_blank" href={infoUrl}>{result.name}</a>
-            <span className="txt-dimmed"> {result.npm.version}
-            </span>
-          </h3>
+          <h2 className="m0 fl-left">
+            <span className="icon icon-hex-4"></span>
+            <a target="_blank" href={infoUrl}>{result.name}</a>
+          </h2>
           {this.hasBuild(result.travis) ?
             <div className="fl-right module-badges">
               <TravisBadge buildState={result.travis.buildState}/>
@@ -32,8 +30,9 @@ export const SearchResult = React.createClass({
               <ModuleSource data={result} sourceName="travis"/>}
           </div>
         </div>
-        <p className="module-description">
-          <i>{result.npm.description}</i>
+        <p className="module-description txt-dimmed">
+          {result.npm.description} <br/>
+          <span className="txt-dimmer txt-small"> v{result.npm.version} Updated 16 days ago </span>
         </p>
         <GithubResult github={result.github}/>
       </div>
