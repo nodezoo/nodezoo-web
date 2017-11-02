6fbf120314ac4d9195d84ff509a53fee5e57c96b
Richard Rodger <richard@ricebridge.com>
2017-11-02 18:39:08 +0000

    consul

--- srv/web-prod.js
+++ srv/web-prod.js
@@ -1,6 +1,8 @@
 /* Copyright (c) 2014-2017 Richard Rodger and other contributors, MIT License */
 
-var BASES = process.env.BASES.split(',')
+//var BASES = process.env.BASES.split(',')
+var CONSUL = process.env.CONSUL_SERVICE_HOST || 'localhost'
+
 
 var Seneca = require('seneca')
 
@@ -9,9 +11,18 @@ var app = require('../web.js')
 Seneca({tag: 'web'})
   .test('print')
 
+  .use('consul-registry', {
+    host: CONSUL
+  })
+
   .use('mesh', {
-    bases: BASES,
+    //bases: BASES,
     host: '@eth0',
+    discover: {
+      registry: {
+        active: true
+      }
+    },
     sneeze: {silent:false}
   })
 
